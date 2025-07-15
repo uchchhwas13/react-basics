52c5158af3aff427a1a5924cf5e07d62d4961a28
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-15 14:00:54 +0600

    increasing count after tapping on card

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 const Card = ({ title, content }) => {
+ const [count, setCount] = useState(0);
   const [hasLiked, setHasliked] = useState(false);
+  
+  useEffect(() => {
+  console.log(`Card with title "${title}" has been liked: "${hasLiked}".`);
+}, [hasLiked]);
+
   return (
-    <div className="card">
-      <h2>{title}</h2>
+    <div className="card" onClick={() => setCount(count + 1)}>
+      <h2>{title} <br/> {count|| null}</h2>
       <p>{content}</p>
       <button onClick={() => setHasliked(!hasLiked)}> 
         {hasLiked ? 'Liked':'Like'}
