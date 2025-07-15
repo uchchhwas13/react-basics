e2b8ebf6d539aa9ba7f53c24c76c33773225ea78
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-15 13:41:28 +0600

    Adding like or unlike button for movie card

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,16 +1,21 @@
-
+import { useState } from 'react'
 import './App.css'
 
 const Card = ({ title, content }) => {
+  const [hasLiked, setHasliked] = useState(false);
   return (
     <div className="card">
       <h2>{title}</h2>
       <p>{content}</p>
+      <button onClick={() => setHasliked(!hasLiked)}> 
+        {hasLiked ? 'Liked':'Like'}
+      </button>
     </div>
   )
 }
 
 const App = () => {
+
   return (
     <div className="card-container">
       <Card title="Star wars" content="Great movie" />
