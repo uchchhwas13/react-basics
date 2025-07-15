1ec99ab18e6dd615fb76da57d469bef15df8671f
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-15 15:49:02 +0600

    Fetching movie data using network call

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,9 +1,45 @@
 import React from 'react';
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import Search from './components/search';
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const fetchMovies = async() => {
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      console.log(data);
+      if (data.response === 'False') {
+        setErrorMessage(data.error || 'Failed to fetch movies');
+      }
+
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Failed to fetch movies. Please try again later.');
+    }
+  }
+  
+  useEffect(() => { 
+    fetchMovies();
+  }, []);
 
   return (
     <main>
@@ -13,9 +49,12 @@ const App = () => {
           <img src="./hero.png" alt="Hero Banner" />
           <h1> Find <span className="text-gradient">Movies</span> You'll Enjoy without Hassle
           </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-        <h1 className="text-white">{searchTerm}</h1>
+        <section className="all-movies"> 
+          <h2> All movies</h2>
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+          </section>
       </div>
     </main>
   );
