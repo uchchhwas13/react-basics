222e46b29569941e4b8af3b9dede04e414be77d7
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-15 12:26:02 +0600

    Adding initial styles for card and card-container

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Card = ({ title, content }) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="card">
+      <h2>{title}</h2>
+      <p>{content}</p>
+    </div>
   )
 }
+
+const App = () => {
+  return (
+    <div className="card-container">
+      <Card title="Star wars" content="Great movie" />
+      <Card title="Star trek" content="Awesome movie" />
+      <Card title="Star Avengers" content="Best superhero movie" />
+    </div>
+)}
 
 export default App
