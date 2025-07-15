f3c97548069ca423e6a43b5f19632bb8bee6dec4
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-15 18:00:09 +0600

    Adding appwrite file and importing env variables

---
+++ react-basics/src/appwrite.js
@@ -0,0 +1,3 @@
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
