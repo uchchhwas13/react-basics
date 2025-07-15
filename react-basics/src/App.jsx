c572c0eba8f5ca6caa97d99998bf48cb527212eb
Uchchhwas Roy <uchchhwas.roy.du@gmail.com>
2025-07-15 16:07:09 +0600

    Dropping unnecessary empty string

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -55,7 +55,6 @@ const App = () => {
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>
-            {' '}
             Find <span className="text-gradient">Movies</span> You'll Enjoy
             without Hassle
           </h1>
